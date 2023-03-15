146d5dafed8436afd24f470096cf6721094f2322
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-15 21:55:46 +0600

    quiz question not showing bug fixed

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ console.log(data);
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
