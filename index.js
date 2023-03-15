0834d332ef6e1a922c79be445f2eaaf12cf53e08
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-15 22:49:09 +0600

    previous result shoing table bug fixed

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
   if (storage) {
     localStorage.setItem(
       "results",
